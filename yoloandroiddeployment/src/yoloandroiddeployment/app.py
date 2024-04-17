73d69d339772dd1d23a4287496efb6791c7957de
vip30service123 <80677107+vip30service123@users.noreply.github.com>
2024-04-17 10:59:18 +0700

    capture picture

--- yoloandroiddeployment/src/yoloandroiddeployment/app.py
+++ yoloandroiddeployment/src/yoloandroiddeployment/app.py
@@ -17,24 +17,30 @@ class YoloAndroidDeployment(toga.App):
         We then create a main window (with a name matching the app), and
         show the main window.
         """
-        # Capture frame-by-frame
-        cap = cv.VideoCapture(0)
-
-        ret, frame = cap.read()
-        # Display the resulting frame
-        im = Image.fromarray(frame)
-
         main_box = toga.Box()
-        # my_image = toga.Image("image.jpg")
-        view = toga.ImageView(im)
-        main_box.add(view)
+
+        self.view = toga.ImageView()
+        main_box.add(self.view)
+
+        capture_button = toga.Button(
+            "Capture",
+            on_press=self.capture,
+        )
+        main_box.add(capture_button)
 
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
 
+    def capture(self, widget):
+        cap = cv.VideoCapture(0)
 
+        ret, frame = cap.read()
+
+        im = Image.fromarray(frame)
         
+        self.view.image = im
+    
 
 def main():
     return YoloAndroidDeployment()
